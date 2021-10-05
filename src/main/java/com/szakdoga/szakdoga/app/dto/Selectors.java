7004039f38c0282acf697d0d90cfefd4c585043f
Norbert Sikorszki <sikorszki18@gmail.com>
2021-10-05 10:13:52 +0200

    WebScrape

---
+++ src/main/java/com/szakdoga/szakdoga/app/dto/Selectors.java
@@ -0,0 +1,13 @@
+package com.szakdoga.szakdoga.app.dto;
+
+import lombok.Data;
+
+@Data
+public class Selectors {
+
+    String emagSelector = "nav .product-new-price";
+    String goldDekorSelector = "table .product_table_price";
+    String eDigital = "article .price-wrapper .price";
+    String praktiker = "section .price";
+
+}
